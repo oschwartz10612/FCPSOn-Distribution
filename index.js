0b48000c1cae408bf9717d8d6e25abb843003876
Schwartz <1439726@fcps.edu>
2019-08-06 11:30:40 -0400

    revised look and local bootstrap

--- index.js
+++ index.js
@@ -9,9 +9,18 @@ app.use(express.json());
 
 app.use(express.static('public'));
 app.use(express.static(__dirname + '/node_modules/socket.io-client/dist'));
+app.use(express.static(__dirname + '/node_modules/bootstrap/dist'));
+app.use(express.static(__dirname + '/node_modules/jquery/dist'));
+
 
 app.get('/', function(req, res) {
-    res.render('pages/index');
+    res.render('pages/4tiles');
+});
+app.get('/4', function(req, res) {
+    res.render('pages/4tiles');
+});
+app.get('/6', function(req, res) {
+    res.render('pages/6tiles');
 });
   
 app.post('/update', function(req, res) {
@@ -27,22 +36,34 @@ app.post('/update', function(req, res) {
     if (req.body.kiosk == '4') {
         io.emit('kiosk4', req.body.location);   
     }
+    if (req.body.kiosk == '5') {
+        io.emit('kiosk5', req.body.location);   
+    }
+    if (req.body.kiosk == '6') {
+        io.emit('kiosk6', req.body.location);   
+    }
     res.send('Sucsess!');
 });
 
 io.on('connection', function(client) {
      client.on('kiosk1', function(data) {
         io.emit('kiosk1', data);   
-     });
+    });
     client.on('kiosk2', function(data) {
         io.emit('kiosk2', data);   
-     });
+    });
     client.on('kiosk3', function(data) {
         io.emit('kiosk3', data);   
-     });
+    });
     client.on('kiosk4', function(data) {
         io.emit('kiosk4', data);   
-     });
+    });
+    client.on('kiosk5', function(data) {
+        io.emit('kiosk5', data);   
+    });
+    client.on('kiosk6', function(data) {
+        io.emit('kiosk6', data);   
+    });
  });
  
 
